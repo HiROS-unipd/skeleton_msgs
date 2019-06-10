a2b5feabdbc1914d32c40fcce2e056f55b4c790c
Mattia Guidolin <mattia.guidolin@gmail.com>
2019-06-10 11:45:06 +0200

    Add skeleton types and conversion utils
    ROS msg to struct, struct to ROS msg

---
+++ include/skeleton_msgs/types.h
@@ -0,0 +1,96 @@
+#ifndef hiros_skeleton_msgs_types_h
+#define hiros_skeleton_msgs_types_h
+
+#include <iostream>
+#include <limits>
+#include <string>
+#include <vector>
+
+namespace hiros {
+    namespace skeletons {
+        namespace types {
+
+            // Point
+            struct Point
+            {
+                Point(const double& t_x = std::numeric_limits<double>::quiet_NaN(),
+                      const double& t_y = std::numeric_limits<double>::quiet_NaN(),
+                      const double& t_z = std::numeric_limits<double>::quiet_NaN());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const Point& t_p);
+
+                double x, y, z;
+            };
+
+            // Rectangle
+            struct Rectangle
+            {
+                Rectangle(const double& t_x = std::numeric_limits<double>::quiet_NaN(),
+                          const double& t_y = std::numeric_limits<double>::quiet_NaN(),
+                          const double& t_width = std::numeric_limits<double>::quiet_NaN(),
+                          const double& t_height = std::numeric_limits<double>::quiet_NaN());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const Rectangle& t_r);
+
+                double x, y, width, height;
+            };
+
+            // Keypoint
+            struct Keypoint
+            {
+                Keypoint(const unsigned& t_id = std::numeric_limits<unsigned>::quiet_NaN(),
+                         const double& t_confidence = std::numeric_limits<double>::quiet_NaN(),
+                         const Point& t_point = Point());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const Keypoint& t_k);
+
+                unsigned id;
+                double confidence;
+                Point point;
+            };
+
+            // KeypointGroup
+            struct KeypointGroup
+            {
+                KeypointGroup(
+                    const unsigned& t_id = std::numeric_limits<unsigned>::quiet_NaN(),
+                    const double& t_confidence = std::numeric_limits<double>::quiet_NaN(),
+                    const std::vector<Keypoint>& t_keypoints = std::vector<Keypoint>(),
+                    const std::vector<Rectangle>& t_boundingBoxes = std::vector<Rectangle>());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const KeypointGroup& t_kg);
+
+                unsigned id;
+                double confidence;
+                std::vector<Rectangle> boundingBoxes;
+                std::vector<Keypoint> keypoints;
+            };
+
+            // Skeleton
+            struct Skeleton
+            {
+                Skeleton(const unsigned& t_id = std::numeric_limits<unsigned>::quiet_NaN(),
+                         const std::vector<KeypointGroup>& t_skeletonParts =
+                             std::vector<KeypointGroup>());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const Skeleton& t_s);
+
+                unsigned id;
+                std::vector<KeypointGroup> skeletonParts;
+            };
+
+            // SkeletonGroup
+            struct SkeletonGroup
+            {
+                SkeletonGroup(const std::vector<Skeleton>& t_skeletons = std::vector<Skeleton>());
+
+                friend std::ostream& operator<<(std::ostream& t_os, const SkeletonGroup& t_sg);
+
+                std::vector<Skeleton> skeletons;
+            };
+
+        } // namespace types
+    } // namespace skeletons
+} // namespace hiros
+
+#endif
---
+++ include/skeleton_msgs/utils.h
@@ -0,0 +1,95 @@
+#ifndef hiros_skeletons_utils_h
+#define hiros_skeletons_utils_h
+
+#include "skeleton_msgs/Keypoint.h"
+#include "skeleton_msgs/KeypointGroup.h"
+#include "skeleton_msgs/Rectangle.h"
+#include "skeleton_msgs/Skeleton.h"
+#include "skeleton_msgs/SkeletonGroup.h"
+#include "skeleton_msgs/types.h"
+
+#include <geometry_msgs/Point.h>
+#include <limits>
+#include <ros/ros.h>
+
+namespace hiros {
+    namespace skeletons {
+        namespace utils {
+
+            // Point
+            hiros::skeletons::types::Point
+            toStruct(const double& t_x,
+                     const double& t_y,
+                     const double& t_z = std::numeric_limits<double>::quiet_NaN());
+
+            hiros::skeletons::types::Point toStruct(const geometry_msgs::Point& t_p);
+
+            geometry_msgs::Point toMsg(const hiros::skeletons::types::Point& t_p);
+
+            // Rectangle
+            hiros::skeletons::types::Rectangle toStruct(const double& t_x,
+                                                        const double& t_y,
+                                                        const double& t_width,
+                                                        const double& t_height);
+
+            hiros::skeletons::types::Rectangle toStruct(const skeleton_msgs::Rectangle& t_r);
+
+            skeleton_msgs::Rectangle toMsg(const hiros::skeletons::types::Rectangle& t_r);
+
+            // Keypoint
+            hiros::skeletons::types::Keypoint
+            toStruct(const unsigned& t_id,
+                     const double& t_confidence,
+                     const double& t_x,
+                     const double& t_y,
+                     const double& t_z = std::numeric_limits<double>::quiet_NaN());
+
+            hiros::skeletons::types::Keypoint
+            toStruct(const unsigned& t_id,
+                     const double& t_confidence,
+                     const hiros::skeletons::types::Point& t_point);
+
+            hiros::skeletons::types::Keypoint toStruct(const skeleton_msgs::Keypoint& t_k);
+
+            skeleton_msgs::Keypoint toMsg(const hiros::skeletons::types::Keypoint& t_k);
+
+            // keypointGroup
+            hiros::skeletons::types::KeypointGroup
+            toStruct(const unsigned& t_id,
+                     const double& t_confidence,
+                     const std::vector<hiros::skeletons::types::Rectangle>& t_boundingBoxes,
+                     const std::vector<hiros::skeletons::types::Keypoint> t_keypoints);
+
+            hiros::skeletons::types::KeypointGroup
+            toStruct(const skeleton_msgs::KeypointGroup& t_kg);
+
+            skeleton_msgs::KeypointGroup toMsg(const hiros::skeletons::types::KeypointGroup& t_kg);
+
+            // Skeleton
+            hiros::skeletons::types::Skeleton
+            toStruct(const unsigned& t_id,
+                     const std::vector<hiros::skeletons::types::KeypointGroup>& t_skeletonParts);
+
+            hiros::skeletons::types::Skeleton toStruct(const skeleton_msgs::Skeleton& t_s);
+
+            skeleton_msgs::Skeleton toMsg(const hiros::skeletons::types::Skeleton& t_s);
+
+            // SkeletonGroup
+            hiros::skeletons::types::SkeletonGroup
+            toStruct(const std::vector<hiros::skeletons::types::Skeleton>& t_skeletons);
+
+            hiros::skeletons::types::SkeletonGroup
+            toStruct(const skeleton_msgs::SkeletonGroup& t_sg);
+
+            skeleton_msgs::SkeletonGroup toMsg(const hiros::skeletons::types::SkeletonGroup& t_sg);
+
+            skeleton_msgs::SkeletonGroup toMsg(const unsigned& t_seq,
+                                               const ros::Time& t_stamp,
+                                               const std::string& t_frame_id,
+                                               const ros::Time& t_srcTime,
+                                               const hiros::skeletons::types::SkeletonGroup& t_sg);
+
+        } // namespace utils
+    } // namespace skeletons
+} // namespace hiros
+#endif
